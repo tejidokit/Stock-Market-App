30704b12c461c4ead2491183cc3f15327ad1ce97
Kit Tejido <tejidokit@gmail.com>
2019-12-28 13:16:07 -0700

    initial commit

--- app.js
+++ app.js
@@ -19,7 +19,33 @@ app.get('/', function (req, res) {
     });
 });
 
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about',);
+});
+
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
+
+/* Git commands used
+ git config --global user.name "Kit Tejido"
+ git config --global user.email "tejidokit@gmail.com"
+ git config --global push.default matching
+ git config --global alias.co checkout
+ git init 
+ git commit -am 'initial commit'
+ ls -a
+ mkdir ~/.ssh
+ cd ~/.ssh
+ ssh-keygen.exe
+ ls
+ cat id_rsa.pub
+ git remote add origin https://github.com/tejidokit/nodejsstockmarketapp.git
+ git push -u origin master
+ git add .
+ git commit -am 'added something yo'
+ git push*/
+
+
